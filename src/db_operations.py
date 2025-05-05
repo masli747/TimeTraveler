96961615a08afed8d31332ca079a644cfeed9a81
Mason Li <masli@chapman.edu>
2025-05-05 14:19:49 -0700

    Implemented connection initialization and closure in db_operations

--- src/db_operations.py
+++ src/db_operations.py
@@ -4,10 +4,37 @@
  * a password file and connect to the MySQL database.
 """
 
+import json
 import mysql.connector
 
 class db_operations:
+    # Make a connection to the MySQL database upon creation so we can conduct DML/DQL.
     def __init__(self):
-        # read username + password from file
-        # make mysql connection
+        # Get properties for connecting to server.
+        config = get_mysql_config()
+        
+        self.connection = mysql.connector.connect(
+            host = config.get('host', ''),
+            user = config.get('user', ''),
+            password = config.get('password', ''),
+            auth_plugin = 'mysql_native_password',
+            database = config.get('database', '')
+        )
+
+        self.cursor = self.connection.cursor()
+
+        print('connection made...')
+
         return
+    
+    # Load MySQL configuration from configuration file.
+    def get_mysql_config(self):
+        with open('config.json', 'r') as file:
+            config = json.load(file)
+        return config.get('mysql', {})
+    
+    # On deletion, clean up connection.
+    def destructor(self):
+        self.cursor.close()
+        self.connection.close()
+        print("connection closed... goodbye!")
